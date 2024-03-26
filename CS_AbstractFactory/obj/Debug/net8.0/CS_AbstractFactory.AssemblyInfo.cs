76e05ca3aac572de0734afeb644f5f68c551ee00
Soriano, Paulo Amiel <pauloamiel.soriano@dyson.com>
2024-03-26 16:40:21 +0000

    removed assemblies and executables

--- CS_AbstractFactory/obj/Debug/net8.0/CS_AbstractFactory.AssemblyInfo.cs
+++ CS_AbstractFactory/obj/Debug/net8.0/CS_AbstractFactory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS_AbstractFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4995a2c5e830d072f2181b513ab9c4456e27967a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS_AbstractFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS_AbstractFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CS_AbstractFactory_2/obj/Debug/net8.0/CS_AbstractFactory_2.AssemblyInfo.cs
+++ CS_AbstractFactory_2/obj/Debug/net8.0/CS_AbstractFactory_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS_AbstractFactory_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4995a2c5e830d072f2181b513ab9c4456e27967a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS_AbstractFactory_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS_AbstractFactory_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
